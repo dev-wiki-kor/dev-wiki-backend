348944464140ad480819ec807f3aa60c1651c8c5
jaeyoung <kjy0302014@gmail.com>
2023-10-23 00:06:52 +0900

    [feat] JpaConfig 추가

---
+++ core/core-jpa/src/main/java/com/devwiki/backend/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.devwiki.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
